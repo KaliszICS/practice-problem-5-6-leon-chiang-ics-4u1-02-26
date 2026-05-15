12b3c7411dce92af7159eed8696fc1f079fcb7a0
leon-chiang-ics-4u1-02-26 <Leon.Chiang@student.tdsb.on.ca>
2026-05-15 14:10:19 +0000

    mini update 2

--- src/main/java/Smartphone.java
+++ src/main/java/Smartphone.java
@@ -1,3 +1,31 @@
 public class Smartphone {
+
     private Battery battery;
+    private Display display;
+    private Processor processor;
+    private String brand;
+    private String model;
+
+    public Smartphone(String brand, String model, Battery battery, Display display, Processor processor) {
+        this.brand = brand;
+        this.model = model;
+        this.battery = battery;
+        this.display = display;
+        this.processor = processor;
+    }
+
+    public Smartphone(String brand, String model, int batteryCapacity, double displaySize, String resolution, String processorModel, double processorSpeed) {
+        this.brand = brand;
+        this.model = model;
+        this.battery = new Battery(batteryCapacity);
+        this.display = new Display(displaySize, resolution);
+        this.processor = new Processor(processorModel, processorSpeed);
+    }
+
+    public String powerOn() {
+        if (battery.isCharged() == true) {
+            display.turnOn();
+            return "Powering on" + brand + " " + model;
+        }
+    }
 }
